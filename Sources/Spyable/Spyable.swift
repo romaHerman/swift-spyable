67478c3bf3d67593ad2471214b7959019cac43f3
romaherman <romaherman.dev@gmail.com>
2024-02-29 10:56:48 +0200

    added export XCTest to Spyable

--- Sources/Spyable/Spyable.swift
+++ Sources/Spyable/Spyable.swift
@@ -52,6 +52,7 @@
 ///
 /// - NOTE: The `@Spyable` macro should only be applied to protocols. Applying it to other
 ///         declarations will result in an error.
+@_exported import XCTest
 @attached(peer, names: suffixed(Spy))
 public macro Spyable(behindPreprocessorFlag: String? = nil) =
   #externalMacro(
